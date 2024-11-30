5159aa159f43a13e19e7604c3d3d0b90804eade8
Timofey Vorobyov <t.vorobyov@gmail.com>
2024-11-30 19:06:52 +0200

    Round commissions up to 2 decimal places

--- app/Service/CommissionCalculator.php
+++ app/Service/CommissionCalculator.php
@@ -6,9 +6,11 @@ use App\Models\CountryData;
 
 class CommissionCalculator implements CommissionInterface
 {
+    private const int DECIMAL_PLACES = 2;
     public function getTransactionCommission(float $amount, string $countryCode): float
     {
-        return $amount * $this->getCommissionRate($countryCode);
+        $roundFactor = pow(10, self::DECIMAL_PLACES);
+        return ceil($roundFactor * $amount * $this->getCommissionRate($countryCode)) / $roundFactor;
     }
 
     private function getCommissionRate(string $country): float
